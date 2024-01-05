9412c0c19c10c69865ec29ed0ecdca41e78806eb
qhu <qiang.hu@roswellpark.org>
2024-01-05 13:13:55 -0500

    update format scripts

--- bin/format_eas.R
+++ bin/format_eas.R
@@ -8,7 +8,6 @@ info_f <- args[2]
 library(tidyverse)
 
 ## info
-## info_f <- list.files(ldm, ".*_chr.*.info$", full.names = TRUE)
 l1 <- read_table(info_f, n_max=1, col_names = FALSE, show_col_types = F)
 if(l1[1,1]!="Chrom"){
     info <- read_table(info_f, col_names = F)
